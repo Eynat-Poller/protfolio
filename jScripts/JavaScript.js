68ab0c33e3365a9280d372cbdfad6fa509a328e6
Eynat-Poller <37344091+Eynat-Poller@users.noreply.github.com>
2018-04-22 18:02:46 +0300

    Add files via upload

---
+++ jScripts/JavaScript.js
@@ -0,0 +1,15 @@
+﻿function openNav() {
+    document.getElementById("mySidenav").style.width = "150px";
+    document.getElementById("main").style.marginright = "150px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.3)";
+}
+
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginright = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+
+
